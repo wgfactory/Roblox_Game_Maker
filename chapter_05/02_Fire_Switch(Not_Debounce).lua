879fb91048927c584ddf81e799828f608c41e65f
WgWgFactory <60998245+wgfactory@users.noreply.github.com>
2020-06-12 17:39:37 +0900

    Create 02_Fire_Switch(Not_Debounce).lua

---
+++ chapter_05/02_Fire_Switch(Not_Debounce).lua
@@ -0,0 +1,16 @@
+local switch = script.Parent -- Swit Part를 part 변수에 담기
+
+function FireSwitch(player) -- FireSwitch 함수 정의
+    local character = player.Parent -- chracter변수에 player담기
+    local humanoid = character:FindFirstChild("Humanoid") -- player가 “Humanoid”인지 판별
+        
+    if humanoid ~= nil then -- 첫번째 if문 조건
+        local fire = Instance.new("Fire") -- fire 변수에 Fire 담기
+        local light = Instance.new("PointLight") -- light 변수에 PointLight 담기
+        fire.Parent = game.Workspace.FireBowl.Bowl.Fire -- Fire Part에 불 붙이기
+        light.Parent = game.Workspace.FireBowl.Bowl -- Bowl Part에 PointLight 붙이기
+    end
+	
+end
+
+switch.Touched:Connect(FireSwitch)
