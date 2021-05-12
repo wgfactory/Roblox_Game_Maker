4abb9af2a567807860bbd41c42c46e45f71d4d25
WgWgFactory <60998245+wgfactory@users.noreply.github.com>
2021-05-12 14:44:47 +0900

    Update 01_fire.lua

--- chapter_05/01_fire.lua
+++ chapter_05/01_fire.lua
@@ -1,7 +1,6 @@
-local part = script.Parent -- Block Part를 part 변수에 담기
-local fire = Instance.new("Fire") -- Fire Class를 fire 변수에 담기
-fire.Parent = part -- Fire Class를 Block Part에 Child로 지정 하여 구현
-
-fire.Size = 15 -- Fire Instance의 Size 속성 변경
-fire.Heat = 15 -- Fire Instance의 Heat 속성 변경
-fire.Color = Color3.fromRGB(0,0,255) -- Fire Instance의 색상 변경(파란색)
+local part = script.Parent -- 블록 파트를 part 변수에 담기
+local fire = Instance.new("Fire") -- 불(=Fire) 효과를 fire 변수에 담기
+fire.Parent = part -- 불(=Fire) 효과의 부모(Parent)를 블록 파트로 지정
+fire.Size = 15 -- 불 효과의 크기(Size) 변경
+fire.Heat = 15 -- 불 효과의 히트(Heat) 변경
+fire.Color = Color3.fromRGB(0,0,255)  -- 불 효과의 색상 변경(파란색)
