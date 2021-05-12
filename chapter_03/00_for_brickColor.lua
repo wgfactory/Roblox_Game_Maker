db517c8341db1ed73724f9d7f6780c62fefe977e
WgWgFactory <60998245+wgfactory@users.noreply.github.com>
2021-05-12 14:04:41 +0900

    Create 00_for_brickColor.lua

---
+++ chapter_03/00_for_brickColor.lua
@@ -0,0 +1,14 @@
+local colorBlock = game.Workspace.ColorPart -- colorBrick 변수에 ColorPart 담기
+
+local red = Color3.fromRGB(255 ,0, 0) -- red 변수에 빨간색 담기
+local green = Color3.fromRGB(0, 255, 0) -- green 변수에 초록색 담기
+local blue = Color3.fromRGB(0, 0, 255) -- blue 변수에 파란색 담기
+
+for count = 0, 10, 1 do -- for문 설정, count변수가 0부터 10까지 1씩 증가
+    colorBlock.Color = red -- ColorPart의 색상을 빨간색으로 변경
+    wait(1)                 -- 1초 쉬기
+    colorBlock.Color = green -- ColorPart의 색상을 초록색으로 변경
+    wait(1)                 -- 1초 쉬기
+    colorBlock.Color = blue -- ColorPart의 색상을 파란색으로 변경
+    wait(1)                 -- 1초 쉬기
+end
