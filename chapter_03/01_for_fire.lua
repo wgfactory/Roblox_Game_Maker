d711183a1cbd6a25a2c9448f25a74ca24e5707e8
WgWgFactory <60998245+wgfactory@users.noreply.github.com>
2021-05-12 13:36:37 +0900

    Update 01_for_fire.lua

--- chapter_03/01_for_fire.lua
+++ chapter_03/01_for_fire.lua
@@ -8,14 +8,14 @@ local blue = Color3.fromRGB(0,0,255)
 for count = 0, 10, 1 do
     colorPart.Color = red
     fire.Color = red
-    fire.Size = 50
-    fire.Heat = 50
+    fire.Size = 30
+    fire.Heat = 25
     wait(1)
 
     colorPart.Color = green
     fire.Color = green
     fire.Size = 25
-    fire.Heat = 25
+    fire.Heat = 20
     wait(1)
 
     colorPart.Color = blue
